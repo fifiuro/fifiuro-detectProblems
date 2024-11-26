71b0860cccf2566e64099b04d53ec4db9fa8659d
fifiuro <fifiuro@gmail.com>
2024-11-26 15:50:17 -0400

    Christian: Vista previa de problema

--- resources/views/problem/previewProblem.blade.php
+++ resources/views/problem/previewProblem.blade.php
@@ -18,13 +18,25 @@
                     @method('POST')
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="problem">Problema <span class="text-danger">*</span></label>
                                     {{ $problem->problem }}
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="zone">Zona <span class="text-danger">*</span></label>
+                                    {{ $problem->zone }}
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="zone">Calle <span class="text-danger">*</span></label>
+                                    {{ $problem->street }}
+                                </div>
+                            </div>
+                            <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="coordinates">Coordenadas <span class="text-danger">*</span></label>
                                     <a href="{{ $problem->coordinates }}" target="_blank">
@@ -32,19 +44,7 @@
                                     </a>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label for="zone">Zona <span class="text-danger">*</span></label>
-                                    {{ $problem->zone }}
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label for="zone">Calle <span class="text-danger">*</span></label>
-                                    {{ $problem->street }}
-                                </div>
-                            </div>
-                            <div class="col-md-12">
+                            <div class="col-md-8">
                                 <div class="form-group">
                                     <label for="other">Otros </label>
                                     {{ $problem->other }}
@@ -64,35 +64,16 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-6">
-                            <h3>Lista de Fotos</h3>
+                        <div class="col-md-12">
                             @foreach ($photo as $key => $d)
-                                <div class="col-md-12">
+                                <div class="col-md-12 pb-4">
                                     <img src="{{ asset('storage/' . $d->path) }}" alt="{{ $d->filename }}"
-                                        style="width:200px; height:auto;">
+                                        style="width:100%; height:auto;">
                                 </div>
                             @endforeach
                         </div>
-                        <div class="col-md-6">Columna de Mensajes</div>
                     </div>
                 </div>
-                <div class="card-footer">
-                    <div class="input-group">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <button type="submit" class="btn btn-primary">
-                                    GUARDAR
-                                </button>
-                            </div>
-                            <div class="col-md-6">
-                                <a href="{{ route('problem.list') }}" class="btn btn-danger">
-                                    CANCELAR
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </form>
             </div>
         </div>
         <div class="col-md-6">
@@ -102,31 +83,30 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-6">
-                            <h3>Lista de Fotos</h3>
-                            @foreach ($photo as $key => $d)
-                                <div class="col-md-12">
-                                    <img src="{{ asset('storage/' . $d->path) }}" alt="{{ $d->filename }}"
-                                        style="width:200px; height:auto;">
-                                </div>
-                            @endforeach
-                        </div>
-                        <div class="col-md-6">Columna de Mensajes</div>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <div class="input-group">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <button type="submit" class="btn btn-primary">
-                                    GUARDAR
-                                </button>
-                            </div>
-                            <div class="col-md-6">
-                                <a href="{{ route('problem.list') }}" class="btn btn-danger">
-                                    CANCELAR
-                                </a>
-                            </div>
+                        <div class="col-md-12">
+                                {{-- {{ dd($comment) }} --}}
+                            <table class="table table-stripped table-hover">
+                                <thead>
+                                    <th>Tipo</th>
+                                    <th>Comentario</th>
+                                    <th>Solución</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($comment as $key => $c)
+                                    <tr>
+                                        <td>
+                                            @if ($c->type == 'C')
+                                                Comentario
+                                            @elseif ($c->type == 'O')
+                                                Oficial
+                                            @endif
+                                        </td>
+                                        <td>{{ $c->comment }}</td>
+                                        <td>{{ $c->solution }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
